198beb0dee8ee37c26dadd86147db21440090319
Rimuru <Rimuru.Dev@gmail.com>
2023-11-29 21:06:46 +0400

    Create PercentageAttribute.cs

---
+++ Sources/Attributes/PercentageAttribute.cs
@@ -0,0 +1,18 @@
+﻿// **************************************************************** //
+//
+//   Copyright (c) RimuruDev. All rights reserved.
+//   For Project: "Murders Drones Endless Way".
+//   Contact me: 
+//          - Gmail:    rimuru.dev@gmail.com
+//          - GitHub:   https://github.com/RimuruDev
+//          - LinkedIn: https://www.linkedin.com/in/rimuru/
+//          - GitHub Organizations: https://github.com/Rimuru-Dev
+//
+// **************************************************************** //
+
+using UnityEngine;
+
+namespace AbyssMoth
+{
+    public class PercentageAttribute : PropertyAttribute {}
+}
